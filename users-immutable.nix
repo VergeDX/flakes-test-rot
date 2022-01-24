d286c81ce9da246c67883836743263e734fa1b30
System administrator <root@NixOS-RoT.vanilla.local>
2022-01-24 10:33:52 +0000

    pref[users]: Add new users vanilla, given the sudo permission.

--- users-immutable.nix
+++ users-immutable.nix
@@ -1,6 +1,15 @@
 { pkgs, ... }:
 {
   users.mutableUsers = false;
-  users.users.root.initialPassword = "hunter2";
+  # users.users.root.initialPassword = "hunter2";
   users.defaultUserShell = pkgs.fish;
+
+  users.users."vanilla" = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  } // {
+    hashedPassword = "$6$rootfs/on/tmpfs" +
+      "$ZdJgQnzRONBdzI0TKoRaXVf5jgvWZMfZL4faLLc1ACMtHuTTzEogZhyBLKB6MsWztkSC6FHygH"
+      + ".6O/o37Pd1p0";
+  };
 }
