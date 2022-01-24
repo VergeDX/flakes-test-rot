70832ee75621a2a4e3abdcc3ddabf6445f28b6f5
System administrator <root@NixOS-RoT.vanilla.local>
2022-01-24 09:48:40 +0000

    pref: Set hostName and domain, allow git commit detect.

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "NixOS-RoT"; # Define your hostname.
+  networking.domain = "vanilla" + "." + "local"; # vanilla.local
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Set your time zone.
