6ca2499d6a44448a0d504e411a63ce0987e834f0
System administrator <root@NixOS-RoT.vanilla.local>
2022-01-24 10:09:09 +0000

    feat[flake-utils]: Simplify NixOS configurations by using flake-utils.

--- flake.nix
+++ flake.nix
@@ -1,9 +1,13 @@
 {
-  inputs.nixos.url = "github:nixos/nixpkgs/nixos-21.11";
-  outputs = { nixos, ... }: {
-    nixosConfigurations."NixOS-RoT" = nixos.lib.nixosSystem {
-      system = "x86_64-linux";
+  inputs = {
+    nixos.url = "github:nixos/nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixos, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system: {
+    packages.nixosConfigurations."NixOS-RoT" = nixos.lib.nixosSystem {
+      inherit system;
       modules = [ ./configuration.nix ];
     };
-  };
+  });
 }
