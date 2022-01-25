313c10781fd896b2b0ce6836884f342ba45e00ad
System administrator <root@NixOS-RoT.vanilla.local>
2022-01-25 02:29:05 +0000

    perf[flake]: Switch to home-manager tag release-21.11.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixos.url = "github:nixos/nixpkgs/nixos-21.11";
     flake-utils.url = "github:numtide/flake-utils";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-21.11";
     nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.inputs.nixpkgs.follows = "nixos-unstable";
   };
