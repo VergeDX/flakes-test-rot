cd63ec46c779b736685f88ebe8ea4cd21edd22d0
System administrator <root@NixOS-RoT.vanilla.local>
2022-01-25 02:14:50 +0000

    feat[home-manager]: Import home-manager, channel follows nixos-unstable.

--- flake.nix
+++ flake.nix
@@ -2,12 +2,17 @@
   inputs = {
     nixos.url = "github:nixos/nixpkgs/nixos-21.11";
     flake-utils.url = "github:numtide/flake-utils";
+
+    home-manager.url = "github:nix-community/home-manager";
+    nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.inputs.nixpkgs.follows = "nixos-unstable";
   };
 
   outputs = { ... }@inputs: inputs.flake-utils.lib.eachDefaultSystem (system: {
     packages.nixosConfigurations."NixOS-RoT" = inputs.nixos.lib.nixosSystem {
       inherit system;
-      modules = [ ./configuration.nix ];
+      modules = with inputs; [ ./configuration.nix ]
+        ++ [ home-manager.nixosModules.home-manager ];
     };
   });
 }
